eed56de923c02ffafc9df36b6f4671dd571884f9
Dejun Qian <dejun@rita.cat.pdx.edu>
2012-11-13 13:58:15 -0800

    arum with binary probe works now. see bin/README file to get howto

---
+++ bin/hello.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <sys/times.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+clock_t fake(struct tms* buf) {
+  static clock_t utime = 10000;
+  static clock_t stime =20000;
+  static clock_t cutime = 30000;
+  static clock_t cstime = 40000;
+  static clock_t ttime = 50000;
+  printf("fake[%p]: %ld\n", buf, utime);
+  buf->tms_utime = utime++;
+  buf->tms_stime = stime++;
+  buf->tms_cutime = cutime++;
+  buf->tms_cstime = cstime++;
+  return ttime++;
+}
+
+void foo(void) {
+  int i;
+  clock_t t1, t2;
+  struct tms time1, time2;
+  printf("foo\n");
+  t1 = times(&time1);
+  volatile int uv = 0;
+  for(i=0; i<20000000; i++) {
+    uv++;
+  }
+  for(i=0; i<200000; i++) {
+    int fd = open("temp.txt", O_CREAT, S_IWUSR);
+    write(fd, &i, sizeof(i));
+    close(fd);
+  }
+  t2 = times(&time2);
+  printf("%jd %jd %jd %jd %jd %jd\n", time1.tms_utime, time2.tms_utime, time1.tms_stime, time2.tms_stime, t1, t2);
+}
+
+int main(void) {
+  int i;
+  printf("hello, world!\n");
+  volatile int uv = 0;
+  for(i=0; i<20000000; i++) {
+    uv++;
+  }
+  for(i=0; i<200000; i++) {
+    int fd = open("temp.txt", O_CREAT, S_IWUSR);
+    write(fd, &i, sizeof(i));
+    close(fd);
+  }
+  foo();
+  return 0;
+}
