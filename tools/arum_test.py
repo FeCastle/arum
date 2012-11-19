9262b0e3ffc48b677d3d469a515b6e67aef01ef4
Sasmita <sasmita@sasmita-VirtualBox.(none)>
2012-11-19 09:39:44 -0800

    Validates Usertime and prints whether testcase is passed or failed

--- tools/arum_test.py
+++ tools/arum_test.py
@@ -1,15 +1,49 @@
+
+# Test driver for arum
+# Author : Sisinty Sasmita Patra
+
 import popen2
 import shlex
 
-fout, fin = popen2.popen2('./Arum Permutation');
+fr = open('testcases_arum.txt', 'r')
 
-fin.close();
+testcases = fr.readlines()
+fr.close()
 
-lines = fout.readlines();
-fout.close();
+# For each test case
+for testcase in testcases:
 
-u = shlex.split(lines[3]);
+	# Parsing the test_name and expected outputs
+	a = shlex.split(testcase);
+	test_name = a[0];
+	expected_usertime = float(a[1]);
 
-usertime = float(u[3]);
+	# Preparing command in the form of : "./Arum TestCase". E.g: "./Arum Permutation"
+	command = './Arum ' + test_name;
 
-print usertime;
+	fout, fin = popen2.popen2(command)
+	fin.close();
+
+	output_lines = fout.readlines();
+	fout.close();
+	
+	for output_line in output_lines:
+		if 'User time' in output_line:
+			u = shlex.split(output_line);
+			usertime  = float(u[3]);
+			
+	# Validating output and expected output
+	if ((usertime - expected_usertime) ** 2) < 0.001:
+                print test_name, " Passed."
+	else:
+		print test_name, " Failed."
+		print "\tTest result: user time: ", usertime
+		print "\t    Expected user time: ", expected_usertime
+
+
+		
+
+
+
+
+		
